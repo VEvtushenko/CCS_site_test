bde937629bca119daeffccfc84e27c399e79c978
VEvtushenko <evt-vladimir@yandex.ru>
2022-06-04 15:05:05 +0300

    fix properties

--- src/test/java/cloud/autotests/config/ProjectConfig.java
+++ src/test/java/cloud/autotests/config/ProjectConfig.java
@@ -3,11 +3,7 @@ package cloud.autotests.config;
 import org.aeonbits.owner.Config;
 
 @Config.LoadPolicy(Config.LoadType.MERGE)
-@Config.Sources({
-        "system:properties",
-        "classpath:config/local.properties",
-        "classpath:config/remote.properties"
-})
+@Config.Sources("classpath:config/remote.properties")
 public interface ProjectConfig extends Config {
 
     @DefaultValue("chrome")
@@ -16,8 +12,6 @@ public interface ProjectConfig extends Config {
     String browserVersion();
     @DefaultValue("1920x1080")
     String browserSize();
-    String browserMobileView();
     String remoteDriverUrl();
-    String videoStorage();
     String webUrl();
 }
